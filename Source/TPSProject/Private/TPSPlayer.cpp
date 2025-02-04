4c3933a3e1491762435fa126b0ed3c42e2243acb
DESKTOP-A2AQS7S\MSI <technoisekor@gmail.com>
2025-02-04 14:16:28 +0900

    TPS CPP Start

---
+++ Source/TPSProject/Private/TPSPlayer.cpp
@@ -0,0 +1,136 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h"
+#include "../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
+
+// Sets default values
+ATPSPlayer::ATPSPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Mesh에 퀸을 로드해서 넣고싶다.
+	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMesh(TEXT("/ Script / Engine.SkeletalMesh'/Game/Characters/Mannequin_UE4/Meshes/SK_Mannequin.SK_Mannequin'"));
+
+	// 만약 파일읽기가 성공했다면
+	if( TempMesh.Succeeded() )
+	{
+		// 로드한 메시를 넣어주고 싶다.
+		GetMesh()->SetSkeletalMesh(TempMesh.Object);
+
+		// 위치값과 회전값을 반영해주고 싶다.
+		GetMesh()->SetRelativeLocationAndRotation(FVector( 0.0f, 0.0f, -90.0f ), FRotator( 0.0f, -90.0f, 0.0f ) );
+	}
+
+	// TPS카메라를 붙이고 싶다.
+	// SpringArm 컴포넌트 붙이기
+	springArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	springArmComp->SetupAttachment( RootComponent );
+	springArmComp->SetRelativeLocation( FVector( 0.0f, 60.0f, 80.0f ) );
+	springArmComp->TargetArmLength = 300.0f;
+	springArmComp->bUsePawnControlRotation = true;
+
+	// Camera 컴포넌트 붙이기
+	tpsCamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("TpsCamComp"));
+	tpsCamComp->SetupAttachment( springArmComp );
+	tpsCamComp->bUsePawnControlRotation = false;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = false;
+
+
+
+
+
+	
+}
+
+// Called when the game starts or when spawned
+void ATPSPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto pc = Cast<APlayerController>(Controller);
+	if( pc )
+	{
+		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
+
+		if( subsystem )
+		{
+			subsystem->AddMappingContext(IMC_TPS, 0);
+		}
+	}	
+
+	//JumpMaxCount = 3;
+}
+
+// Called every frame
+void ATPSPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// P(결과위치) = P0(현재위치) + v(속도) x t(시간)	
+	Direction = FTransform(GetControlRotation()).TransformVector(Direction);
+	
+	/*
+	FVector P0 = GetActorLocation();
+	FVector vt = Direction * WalkSpeed * DeltaTime;
+	FVector P = P0 + vt;
+	SetActorLocation( P );
+	*/
+	AddMovementInput(Direction);
+
+	Direction = FVector::ZeroVector;
+}
+
+// Called to bind functionality to input
+void ATPSPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	auto PlayerInput = Cast<UEnhancedInputComponent>( PlayerInputComponent );
+	if( PlayerInput )
+	{
+		PlayerInput->BindAction(IA_Turn, ETriggerEvent::Triggered, this, &ATPSPlayer::Turn);
+		PlayerInput->BindAction(IA_LookUp, ETriggerEvent::Triggered, this, &ATPSPlayer::LookUp);
+
+		PlayerInput->BindAction(IA_Move, ETriggerEvent::Triggered, this, &ATPSPlayer::Move);
+
+		PlayerInput->BindAction(IA_Jump, ETriggerEvent::Started, this, &ATPSPlayer::InputJump);
+	}
+
+}
+
+void ATPSPlayer::Turn( const FInputActionValue& inputValue )
+{
+	float value = inputValue.Get<float>();
+	AddControllerYawInput(value);
+}
+
+void ATPSPlayer::LookUp( const FInputActionValue& inputValue )
+{
+	float value = inputValue.Get<float>();
+	AddControllerPitchInput(value);
+}
+
+void ATPSPlayer::Move( const FInputActionValue& inputValue )
+{
+	FVector2D value = inputValue.Get<FVector2D>();
+
+	// 상하 입력 처리
+	Direction.X = value.X;
+
+	// 좌우 입력 처리
+	Direction.Y = value.Y;
+}
+
+void ATPSPlayer::InputJump( const FInputActionValue& inputValue )
+{
+	Jump();
+}
+
