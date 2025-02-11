6be9601c141f4bccb1123361cd5a9a05c7fc186c
DESKTOP-A2AQS7S\MSI <technoisekor@gmail.com>
2025-02-11 13:55:46 +0900

    플레이어 애님인스턴스 추가
    앞뒤 이동 속도와 애니메이션 적용

---
+++ Source/TPSProject/Private/TPSPlayerAnimInstance.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TPSPlayerAnimInstance.h"
+#include "TPSPlayer.h"
+
+void UTPSPlayerAnimInstance::NativeUpdateAnimation( float DeltaSeconds )
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	ATPSPlayer* player = Cast<ATPSPlayer>(TryGetPawnOwner());
+	if( player == nullptr )
+	{
+		return;
+	}
+
+	FVector velocity = player->GetVelocity();
+	FVector forwardVector = player->GetActorForwardVector();
+
+	// 앞 / 뒤 이동속도
+	Speed = FVector::DotProduct(forwardVector, velocity);
+}
---
+++ Source/TPSProject/Public/TPSPlayerAnimInstance.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TPSPlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSPROJECT_API UTPSPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// 매 프레임 갱신되는 함수
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	// 플레이어 이동 속도
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = TPSPlayerAnimInstace)
+	float Speed = 0.0f;
+
+
+
+
+};
